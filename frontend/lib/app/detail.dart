325cf643a07983ef3b4d09a71db24ba9f6c8055e
Slemgrim <slemgrim@gmail.com>
2014-05-06 23:33:18 +0200

    refactor detail.dart

--- frontend/lib/app/detail.dart
+++ frontend/lib/app/detail.dart
@@ -1,34 +1,43 @@
 part of G0;
 
+/**
+ * Shows single image and handles sizing
+ */
 class Detail{
 
+  // DOM Elements
   Element _element;
   Element _cover;
   Element _spinner;
   Element _imageContainer;
-
   Element _footer;
   Element _user;
   Element _channel;
   Element _date;
   Element _source;
 
-  int _windowWidth;
-  ImageElement loadedImage;
+  // Image meta
+  String id;
+  String imageUrl;
+  String source;
+  String user;
+  String channel;
+  int date;
 
-  DateFormat dateFormat = new DateFormat(G0.DATE_FORMAT);
+  int _windowWidth;
+  ImageElement _loadedImage;
+
+  DateFormat _dateFormat = new DateFormat(G0.DATE_FORMAT);
 
   Detail(){
     _getElements();
     _onResize();
-
-    if(_cover != null){
-      _cover.onClick.listen((_) => _hideDetail());
-    }
-
-    window.onResize.listen((_) => _onResize());
+    _eventBindings();
   }
 
+  /**
+   * Search document for neccessary DOM elements
+   */
   void _getElements(){
     _element = querySelector('#container .detail');
     _cover = querySelector('#container .cover');
@@ -41,41 +50,45 @@ class Detail{
     _imageContainer = _element.querySelector('.image-container');
   }
 
+  void _eventBindings(){
+    _cover.onClick.listen((_) => _hideDetail());
+    window.onResize.listen((_) => _onResize());
+  }
+
+  /**
+   * Saves window width on resize and starts image resizing
+   */
   void _onResize(){
     _windowWidth = window.innerWidth;
     _setImageSize();
   }
 
-  void show(Element target){
-    if(_element == null || _cover == null){
-      return;
-    }
+  /**
+   * Takes a [LIElement] from .image-list retrievs meta from it and displays
+   * singe image
+   */
+  void show(LIElement target){
 
-    String id = target.dataset['id'];
-    String imageUrl = target.dataset['image'];
-    String source = target.dataset['source'];
-    String user = target.dataset['user'];
-    String channel = target.dataset['channel'];
-    int date = int.parse(target.dataset['date']) * 1000 ;
+    _retrieveImageMeta(target);
 
     assert(id != null);
     assert(imageUrl != null);
 
-    loadedImage = new ImageElement(src: imageUrl);
-    loadedImage.classes.add('detail-image');
-    loadedImage.onLoad.listen((Event evt) => _showImage(evt.target));
+    _loadedImage = new ImageElement(src: imageUrl);
+    _loadedImage.classes.add('detail-image');
+    _loadedImage.onLoad.listen((Event evt) => _showImage(evt.target));
 
     DateTime imgDate = new DateTime.fromMillisecondsSinceEpoch(date);
 
     _user.innerHtml = user;
     _channel.innerHtml = channel;
-    _date.innerHtml = '${dateFormat.format(imgDate)}';
+    _date.innerHtml = '${_dateFormat.format(imgDate)}';
     _source.innerHtml = source;
     _source.setAttribute('href', source);
 
     window.history.pushState(
         null,
-        'imageUrl',
+        imageUrl,
         window.location.pathname + '?offset=$id'
     );
 
@@ -83,6 +96,20 @@ class Detail{
     _showDetail();
   }
 
+  void _retrieveImageMeta(LIElement target){
+    id = target.dataset['id'];
+    imageUrl = target.dataset['image'];
+    source = target.dataset['source'];
+    user = target.dataset['user'];
+    channel = target.dataset['channel'];
+    date = int.parse(target.dataset['date']) * 1000 ;
+  }
+
+
+  /**
+   * Find [LIElement] by offset and delegates to [show]
+   * This is used for offset from queryString
+   */
   void showByOffset(String offset){
     LIElement target = querySelector('.image-list li[data-id="$offset"]');
     if(target != null){
@@ -94,7 +121,6 @@ class Detail{
     _spinner.classes.add('show');
     _element.classes.add('show');
     _footer.classes.add('show');
-
   }
 
   void _hideDetail(){
@@ -118,17 +144,20 @@ class Detail{
 
     _setImageSize();
     _imageContainer..innerHtml = ''
-                   ..append(loadedImage);
+                   ..append(_loadedImage);
 
     _spinner.classes.remove('show');
   }
 
+  /**
+   * Calculate and apply optimal image size
+   */
   void _setImageSize(){
-    if(loadedImage == null){
+    if(_loadedImage == null){
       return;
     }
-    int origWidth = int.parse(loadedImage.dataset['width']);
-    int origHeight = int.parse(loadedImage.dataset['height']);
+    int origWidth = int.parse(_loadedImage.dataset['width']);
+    int origHeight = int.parse(_loadedImage.dataset['height']);
 
     int width = origWidth > _windowWidth ? _windowWidth : origWidth;
     double ratio = width / origWidth;
