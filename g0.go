d78a9934d07f0182a0be621f91d73b488be5ff37
Filias Heidt <post@filias.name>
2014-05-07 18:30:45 +0200

    Removed the conf.Conf madness

--- g0.go
+++ g0.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/aimless/g0/api"
 	"github.com/aimless/g0/conf"
 	"github.com/aimless/g0/db"
 	"github.com/aimless/g0/ircbot"
@@ -10,23 +9,15 @@ import (
 	"github.com/aimless/g0/util/img"
 )
 
-type JSONconf struct {
-	Imagepath string
-	Thumbpath string
-	DBpath    string
-	Rest      *Api.Api
-	Bot       *IrcBot.Bot
-}
-
 func main() {
 	Init("init")
-	conf.Conf.Bot.LinkChannel = make(chan IrcBot.Link)
+	conf.Bot.LinkChannel = make(chan IrcBot.Link)
 
-	dbase, _ := db.NewDb(conf.Conf.DBpath)
+	dbase, _ := db.NewDb(conf.DBpath)
 
 	//hässliche blocking schleife ist hässlich
 	for true {
-		link := <-conf.Conf.Bot.LinkChannel
+		link := <-conf.Bot.LinkChannel
 		f, hash, err := util.DownloadImage(link.URL)
 		if err != nil {
 			fmt.Println(err.Error())
@@ -42,6 +33,6 @@ func main() {
 }
 
 func Init(placeholder string) {
-	go conf.Conf.Rest.Run()
-	go conf.Conf.Bot.Run()
+	go conf.Rest.Run()
+	go conf.Bot.Run()
 }
