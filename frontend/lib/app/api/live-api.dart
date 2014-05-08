3a1b4928b3a5c42e6450d665602ab489b19972c0
Slemgrim <slemgrim@gmail.com>
2014-05-08 21:36:51 +0200

    very basic api connection

--- frontend/lib/app/api/live-api.dart
+++ frontend/lib/app/api/live-api.dart
@@ -5,7 +5,32 @@ class LiveApi implements Api {
   String _host;
   LiveApi(this._host);
 
-  Future<Map> getImages({String offset: '', int count: 20}){
+  Future<Map> getImages({String offset: '0', int count: 20}){
+    if(offset == null){
+      offset = '0';
+    }
 
+    String url = '${_host}/${offset}/${count}/index.html';
+    Future requestFuture = HttpRequest.getString(url)
+                          .then(_onDataLoaded)
+                          .catchError(_handleError);
+    return requestFuture;
+  }
+
+  Map _onDataLoaded(String responseString){
+    Map response;
+    try {
+      response = JSON.decode(responseString);
+    } catch(exception, stackTrace) {
+      print("can't decode received JSON");
+      print(responseString);
+      response = null;
+    }
+    return response;
+  }
+
+  void _handleError(var error){
+    print("can't connect to API");
   }
 }
+
