82b4b10156d8badfef2a8e7c460b70aeee965803
aimless <m.hackl@gmail.com>
2014-05-08 19:35:00 +0200

    ircbot can use multiple networks again; syphons links only from channels and forwards the channel name properly in the Link struct instead of #unknow placeholder value

--- ircbot/ircbot.go
+++ ircbot/ircbot.go
@@ -8,6 +8,13 @@ import (
 	"regexp"
 )
 
+var chprefixes = map[uint8]bool{
+	'#': true,
+	'&': true,
+	'!': true,
+	'+': true,
+}
+
 var urlregex = regexp.MustCompile(`((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)`)
 var useCrypto = true
 
@@ -37,28 +44,30 @@ func (b *Bot) Run() {
 	ircCon.UseTLS = true
 	ircCon.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
-	ircErr := ircCon.Connect(b.Connections[0].Address)
-	if ircErr != nil {
-		log.Println(ircErr.Error())
+	for _, i := range b.Connections {
+		ircErr := ircCon.Connect(i.Address)
+		if ircErr != nil {
+			log.Println(ircErr.Error())
+		}
+		ircCon.AddCallback("001", func(e *irc.Event) {
+			for _, j := range i.Channels {
+				log.Println("Joining: " + j + " on " + i.Address)
+				ircCon.Join(j)
+			}
+		})
+		ircCon.AddCallback("PRIVMSG", func(e *irc.Event) {
+			if urlregex.MatchString(e.Message()) {
+				urlString := urlregex.FindStringSubmatch(e.Message())
+				ircCon.Privmsg(e.Arguments[0], ">"+urlString[0])
+
+				if ircch := e.Arguments[0]; chprefixes[ircch[0]] {
+					b.LinkChannel <- Link{urlString[0], "unknownnetwork", ircch, e.Nick}
+				}
+			}
+		})
+		i.Connection = ircCon
+		//ircCon.Loop()
 	}
-	ircCon.AddCallback("001", func(e *irc.Event) {
-		for _, i := range b.Connections[0].Channels {
-			log.Println("Joining: " + i)
-			ircCon.Join(i)
-		}
-
-	})
-	ircCon.AddCallback("PRIVMSG", func(e *irc.Event) {
-		if urlregex.MatchString(e.Message()) {
-			urlString := urlregex.FindStringSubmatch(e.Message())
-			ircCon.Privmsg(e.Arguments[0], ">"+urlString[0])
-			b.LinkChannel <- Link{urlString[0], "unknown", "#unknown", e.Nick}
-		}
-	})
-
-	b.Connections = append(b.Connections, Conn{ircCon, b.Connections[0].Address, b.Connections[0].Channels})
-	ircCon.Loop()
-	log.Printf("IRC loop exited")
 }
 
 func parseIrc(e *irc.Event, ircCon *irc.Connection) {
