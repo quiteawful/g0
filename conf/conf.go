d53e25ac5d9f85a333c863fe0a96935479d5f39d
Filias Heidt <post@filias.name>
2014-05-08 16:43:48 +0200

    removed all imports from conf and instead call conf.Fill with the struct you would like to be filled with values from the config

--- conf/conf.go
+++ conf/conf.go
@@ -3,45 +3,22 @@ package conf
 
 import (
 	"encoding/json"
-	"github.com/aimless/g0/api"
-	"github.com/aimless/g0/db"
-	"github.com/aimless/g0/ircbot"
 	"log"
 	"os"
 )
 
-type JSONconf struct {
-	Imagepath string
-	Thumbpath string
-	Db        *Db.DbConfig
-	Rest      *Api.Api
-	Bot       *IrcBot.Bot
-}
-
-var (
-	Imagepath = ""
-	Thumbpath = ""
-	Data      = new(Db.DbConfig)
-	Rest      = new(Api.Api)
-	Bot       = new(IrcBot.Bot)
-)
-
-var c = new(JSONconf)
-
-func init() {
-	file, _ := os.Open("config.json")
-	decoder := json.NewDecoder(file)
-	err := decoder.Decode(c)
+func Fill(s interface{}) error {
+	file, err := os.Open("config.json")
 	if err != nil {
-		log.Println(err.Error())
-		os.Exit(1)
+		log.Println("Could not open config file", err)
 	}
 
-	Imagepath = c.Imagepath
-	Thumbpath = c.Thumbpath
-	Data = c.Db
-	Rest = c.Rest
-	Bot = c.Bot
+	dec := json.NewDecoder(file)
+	err = dec.Decode(s)
+	if err != nil {
+		log.Println("Config parser: ", err, s)
+		return err
+	}
 
-	log.Println("Parsed the following values: ", Imagepath, Thumbpath, Data, Rest, Bot)
+	return nil
 }
