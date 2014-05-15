3bc929442d42eae0640518b850467fc02e48eb63
Marduk <dr.marduk@gmail.com>
2014-05-15 10:22:45 +0200

    'imgur' 'support' added.

---
+++ util/imgur.go
@@ -0,0 +1,13 @@
+package imgur
+
+import (
+	"fmt"
+)
+
+func GetImagesFromAlbum(id string) (string, error) {
+	return "", nil
+}
+
+func GetImagesFromGallery(id string) (string, error) {
+	return "", nil
+}
